eb3dd75bc75e79b85fa5755563b1cb066aa644fb
EritonNaife <115298702+EritonNaife@users.noreply.github.com>
2022-11-06 04:06:24 +0000

    leson learned

--- src/Request.cpp
+++ src/Request.cpp
@@ -16,7 +16,7 @@ void Request::removeStudent(std::map<Student,std::map<Uc,std::vector<Class>>> st
 
     std::cout << "Enter Student's code: " << "\n";
     std::cin >> student_code;
-    student_code = Utility::numberInput(student_code);
+    student_code = Utility::getCode(student_code);
     std::cout << "Student's name" << "\n";
     std::cin >> name;
     std::cout << "Enter Uc's code (L.EIC001) " << "\n";
@@ -25,6 +25,7 @@ void Request::removeStudent(std::map<Student,std::map<Uc,std::vector<Class>>> st
     std::cout << "Enter the class code (1LEIC01): " << "\n";
     std::cin >> class_code;
     class_code = Utility::stringInput(class_code);
+
     Uc uc(uc_code,class_code);
     Student s(student_code,name);
     Class c(class_code);
@@ -35,7 +36,7 @@ void Request::removeStudent(std::map<Student,std::map<Uc,std::vector<Class>>> st
         auto i = it->second.find(uc);
         if(i != it->second.end()){
             auto x = std::find(i->second.begin(), i->second.end(),c);
-            students_uc_classes.erase(s);
+            i->second.erase(x);
         }
     }
 
@@ -58,7 +59,7 @@ void Request::add(std::map<Uc,std::map<Class,std::vector<Slot>>> schedules,std::
 
     std::cout << "Enter Student's code: " << "\n";
     std::cin >> student_code;
-    student_code = Utility::numberInput(student_code);
+    student_code = Utility::getCode(student_code);
     std::cout << "Student's name" << "\n";
     std::cin >> name;
     std::cout << "Enter Uc's code (L.EIC001) " << "\n";
@@ -113,26 +114,27 @@ void Request::add(std::map<Uc,std::map<Class,std::vector<Slot>>> schedules,std::
 
 void Request::change(std::map<Student, std::map<Uc, std::vector<Class>>> students_uc_classes) {
     unsigned int student_code,difference,c1,c2;
-    std::string class_code,name,uc_code,turma;
+    std::string class_code,name,uc_code,antiga_turma;
 
 
     std::cout << "Enter Student's code: " << "\n";
     std::cin >> student_code;
-    student_code = Utility::numberInput(student_code);
+    student_code = Utility::getCode(student_code);
     std::cout << "Student's name" << "\n";
     std::cin >> name;
     std::cout << "Enter Uc's code (L.EIC001) " << "\n";
     std::cin >> uc_code;
     uc_code = Utility::stringInput(uc_code);
-    std::cout << "Enter the class code (1LEIC01): " << "\n";
+    std::cout << "Enter the class code of the class you wish to change (1LEIC01): " << "\n";
     std::cin >> class_code;
+    class_code = Utility::stringInput(class_code);
 
     Student s(student_code,name);
     Uc uc(uc_code,class_code);
     Uc classe_do_estudante(uc_code);
-    Uc old(uc_code,turma);
+
     Class c(class_code);
-    Class class_student(turma);
+
 
     c1 =  c.getClassSize();
 
@@ -142,10 +144,13 @@ void Request::change(std::map<Student, std::map<Uc, std::vector<Class>>> student
         auto it = itr->second.find(classe_do_estudante);
         auto i = it->second.begin();
         if(it!=itr->second.end()){
-            turma = i->getClassCode();
+            antiga_turma = i->getClassCode();
             c2 = i->getClassSize();
         }
     }
+
+    Uc old(uc_code,antiga_turma);
+    Class class_student(antiga_turma);
 
     difference = c2-c1;
 
@@ -159,6 +164,7 @@ void Request::change(std::map<Student, std::map<Uc, std::vector<Class>>> student
         else{
             if(difference >= 4){
                 std::cout << "Unable to change class." << "\n";
+                r.push_back(2);
             }
             else
                 Class::addStudent(uc,s);
@@ -184,46 +190,43 @@ void Request::change(std::map<Student, std::map<Uc, std::vector<Class>>> student
 }
 
 void Request::changeMultiple(std::map<Student, std::map<Uc, std::vector<Class>>> students_uc_classes) {
-    unsigned int student_code,n,c1,c2,difference;
-    std::string class_code,name,uc_code,turma;
-    std:vector<Uc> classes;
-    std::vector<Class> turmas;
+    unsigned int student_code,difference,c1,c2,n;
+    std::string class_code,name,uc_code,antiga_turma;
 
-    std::cout << "Enter Student's code" << "\n";
+
+    std::cout << "Enter Student's code: " << "\n";
     std::cin >> student_code;
-    student_code = Utility::numberInput(student_code);
+    student_code = Utility::getCode(student_code);
     std::cout << "Student's name" << "\n";
     std::cin >> name;
-    std::cout << "Enter the number of classes you wish to change:" << "\n";
+    std::cout << "Enter Uc's code (L.EIC001) " << "\n";
+    std::cin >> uc_code;
+    uc_code = Utility::stringInput(uc_code);
+    std::cout << "Enter the number of classes: " << "\n";
     std::cin >> n;
-    n = Utility::numberInput(n);
+    n = Utility::getInput(n,1,6);
 
     Student s(student_code,name);
+    Uc uc(uc_code,class_code);
     Uc classe_do_estudante(uc_code);
+    Class c(class_code);
 
-    for(unsigned int i = 0 ;i <=n ;i++){
-        std::cout << "Enter the uc code: " << "\n";
-        std::cin >> uc_code;
-        uc_code = Utility::stringInput(uc_code);
-        std::cout << "Enter the class code: " << "\n";
-        std::cin >> class_code;
-        class_code = Utility::stringInput(class_code);
-        Uc uc(uc_code,class_code);
-        Uc old(uc_code,turma);
-        Class c(class_code);
-        Class class_student(turma);
-        c1 =  c.getClassSize();
+    c1 =  c.getClassSize();
 
+    for(int i = 0;i<=n;i++){
         auto itr = students_uc_classes.find(s);
 
         if(itr!=students_uc_classes.end()){
             auto it = itr->second.find(classe_do_estudante);
             auto i = it->second.begin();
             if(it!=itr->second.end()){
-                turma = i->getClassCode();
+                antiga_turma = i->getClassCode();
                 c2 = i->getClassSize();
             }
         }
+
+        Uc old(uc_code,antiga_turma);
+        Class class_student(antiga_turma);
 
         difference = c2-c1;
 
@@ -237,6 +240,7 @@ void Request::changeMultiple(std::map<Student, std::map<Uc, std::vector<Class>>>
             else{
                 if(difference >= 4){
                     std::cout << "Unable to change class." << "\n";
+                    r.push_back(2);
                 }
                 else
                     Class::addStudent(uc,s);
@@ -259,8 +263,8 @@ void Request::changeMultiple(std::map<Student, std::map<Uc, std::vector<Class>>>
                     cout << "Change done successfully ";
             }
         }
-    }
 
+    }
 }
 
 std::vector<int> Request::getUnsuccessufull() {
